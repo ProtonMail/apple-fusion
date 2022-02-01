a133a99944ecca5b82ae80bf2fa6419a8e37ce2b
Aram Sargsyan <aram.sargsyan.1997@gmail.com>
2022-02-01 17:00:48 +0400

    Improved some code and added comments to public functionality

--- pmtest/Recording/VideoGeneration/VideoGenerator.swift
+++ pmtest/Recording/VideoGeneration/VideoGenerator.swift
@@ -1,12 +1,36 @@
 //
 //  VideoGenerator.swift
-//  pmtest
 //
-//  Created by Aram Sargsyan on 26.01.22.
+//  ProtonMail - Created on 26.01.22.
 //
+//  The MIT License
+//
+//  Copyright (c) 2020 Proton Technologies AG
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import UIKit
 import Photos
+
+protocol PixelBufferAppending {
+    func appendPixelBuffers(writer: VideoWriter) -> (isFinished: Bool, success: Bool)
+}
 
 final class VideoGenerator {
 
@@ -38,20 +62,23 @@ final class VideoGenerator {
             completion(success)
         }
     }
+}
 
-    func appendPixelBuffers(writer: VideoWriter) -> AppendPixelBuffersOutput {
+extension VideoGenerator: PixelBufferAppending {
+
+    func appendPixelBuffers(writer: VideoWriter) -> (isFinished: Bool, success: Bool) {
         let frameDuration = CMTimeMakeWithSeconds(configuration.frameDurationInSeconds, preferredTimescale: configuration.timescale)
         while !images.isEmpty {
             if writer.isReadyForData == false {
-                return .init(isFinished: false, success: true)
+                return (isFinished: false, success: true)
             }
             let image = images.removeFirst()
             let presentationTime = CMTimeMultiply(frameDuration, multiplier: Int32(currentFrameNumber))
             if !videoWriter.addImage(image: image, withPresentationTime: presentationTime) {
-                return .init(isFinished: true, success: false)
+                return (isFinished: true, success: false)
             }
             currentFrameNumber += 1
         }
-        return .init(isFinished: true, success: true)
+        return (isFinished: true, success: true)
     }
 }
--- pmtest/Recording/XCUITestCaseRecorder.swift
+++ pmtest/Recording/XCUITestCaseRecorder.swift
@@ -1,13 +1,33 @@
 //
 //  XCUITestCaseRecorder.swift
-//  pmtest
 //
-//  Created by Aram Sargsyan on 26.01.22.
+//  ProtonMail - Created on 26.01.22.
 //
+//  The MIT License
+//
+//  Copyright (c) 2020 Proton Technologies AG
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import XCTest
 
-public class XCUITestCaseRecorder {
+class XCUITestCaseRecorder {
 
     struct Constants {
         static let minimumRequiredScreenshotSize: CGSize = .init(width: 10, height: 10)
@@ -20,9 +40,13 @@ public class XCUITestCaseRecorder {
     private var screenshots = [UIImage]()
     var timeInterval: TimeInterval = 0.3
 
+    // MARK: - Initialization
+
     init(testName: String) {
         self.testName = testName
     }
+
+    // MARK: - Public
 
     func resumeRecording() {
         self.screenshotTimer = Timer.scheduledTimer(timeInterval: timeInterval,
