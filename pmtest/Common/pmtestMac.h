6dc07839592b2d0f20e9c8ffec46c43aad083c81
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2023-01-31 16:10:02 +0100

    Add separate header for macOS target

---
+++ pmtest/Common/pmtestMac.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double pmtestVersionNumber;
+FOUNDATION_EXPORT const unsigned char pmtestVersionString[];
+
+#import <pmtestMac/RunLoopRunUntil.h>
