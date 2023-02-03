4514a883233eaa007646e06f53411748330e19b6
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2023-02-03 07:27:49 +0100

    Clean up `RunLoopRunUntil` implementation

--- pmtest/Common/Waits/RunLoopRunUntil.m
+++ pmtest/Common/Waits/RunLoopRunUntil.m
@@ -3,31 +3,25 @@
 BOOL __attribute__((overloadable))RunLoopRunUntil(NSTimeInterval timeout, BOOL(^condition)(void))
 {
     __block BOOL fulfilled = NO;
+
     void (^beforeWaiting) (CFRunLoopObserverRef observer, CFRunLoopActivity activity) = ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
-        // The run loop should be stopped after the condition is fulfilled (CFRunLoopStop below)
-        NSCParameterAssert(!fulfilled);
+        // Exit `beforeWaiting` block when condition is fulfilled
+        if (fulfilled) return;
+
         // Check the condition
         fulfilled = condition();
+
         // Condition fulfilled: stop RunLoop now
         if (fulfilled) CFRunLoopStop(CFRunLoopGetCurrent());
     };
 
-    // We add a timer dispatch source here to make sure that we wake up at least every 0.x seconds
-    // in case we're waiting for a condition that does not necessarily wake up the run loop.
-    const dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_current_queue());
-    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, (uint64_t)(0.05 * NSEC_PER_SEC), (uint64_t)(0.05 * NSEC_PER_SEC));
-    dispatch_source_set_event_handler(timer, ^{
-        // NOOP
-    });
-    dispatch_resume(timer);
-
     CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(NULL, kCFRunLoopBeforeWaiting, true, 0, beforeWaiting);
     CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
+
     CFRunLoopRunInMode(kCFRunLoopDefaultMode, timeout, false);
+
     CFRunLoopRemoveObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
     CFRelease(observer);
-
-    dispatch_source_cancel(timer);
 
     // If we haven't fulfilled the condition yet, test one more time before returning. This avoids
     // that we fail the test just because we somehow failed to properly poll the condition, e.g. if
