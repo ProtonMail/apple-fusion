e2e45f3b8f7f99507a1831b48ffc6a1e336fc758
Maciej Gomółka <maciej.gomolka@proton.ch>
2023-02-08 13:32:43 +0100

    Make `waitUntil` result discardable

--- pmtest/Common/Waits/WaitUntil.swift
+++ pmtest/Common/Waits/WaitUntil.swift
@@ -6,6 +6,7 @@
  - parameter condition: condition that has to be met to finish `waitUntil` execution before timeout
  - Returns: `Bool` which determines whether the condition is met or not
  */
+@discardableResult
 public func waitUntil(timeout: TimeInterval, condition: @autoclosure @escaping () -> Bool) -> Bool {
     RunLoopRunUntil(timeout, condition)
 }
